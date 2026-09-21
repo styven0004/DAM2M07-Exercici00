cfad352af17a3d85eaf5e27ad9851b9703451a47
styven0004 <styvencatagua@gmail.com>
2026-09-21 18:40:02 +0200

    Controlador i clase principal

--- src/main/java/com/project/Controller.java
+++ src/main/java/com/project/Controller.java
@@ -1,23 +1,55 @@
 package com.project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.text.Text;
-import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
     @FXML
-    private Button buttonAdd;
+    private TextField txtNum1;
 
     @FXML
-    private Text textCounter;
-
-    private int counter = 0;
+    private TextField txtNum2;
 
     @FXML
-    private void actionAdd(ActionEvent event) {
-        counter++;
-        textCounter.setText(String.valueOf(counter));
+    private Label lblResultat;
+
+    @FXML
+    private void operar(ActionEvent event) {
+        try {
+            double num1 = Double.parseDouble(txtNum1.getText());
+            double num2 = Double.parseDouble(txtNum2.getText());
+            double resultat = 0;
+
+            Button btn = (Button) event.getSource();
+            String operacio = btn.getText();
+
+            switch (operacio) {
+                case "+":
+                    resultat = num1 + num2;
+                    break;
+                case "-":
+                    resultat = num1 - num2;
+                    break;
+                case "*":
+                    resultat = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 == 0) {
+                        lblResultat.setText("Error: Divisió per zero");
+                        return;
+                    }
+                    resultat = num1 / num2;
+                    break;
+            }
+
+            lblResultat.setText("Resultat: " + resultat);
+
+        } catch (NumberFormatException e) {
+            lblResultat.setText("Error: Introdueix números vàlids");
+        }
     }
 }
--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -4,32 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    final int WINDOW_WIDTH = 600;
-    final int WINDOW_HEIGHT = 400;
-
     @Override
-    public void start(Stage stage) throws Exception {
-
-        // Carrega la vista inicial des del fitxer FXML
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/assets/layout.fxml"));
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
-        stage.setTitle("JavaFX App");
-        stage.setWidth(WINDOW_WIDTH);
-        stage.setHeight(WINDOW_HEIGHT);
-        stage.show();
-
-        // Afegeix una icona només si no és un Mac
-        if (!System.getProperty("os.name").contains("Mac")) {
-            Image icon = new Image("file:icons/icon.png");
-            stage.getIcons().add(icon);
-        }
+        primaryStage.setTitle("Calculadora Senzilla");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
